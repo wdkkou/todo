84141c0aac704e9ef1fe1e77fa9c61b92ca652c5
wdkkou <koukouki7547@gmail.com>
2019-05-09 22:15:49 +0900

    title 表示

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Helmet} from 'react-helmet';
 import './App.css';
 
-const title = 'todoアプリを作るぞ！';
+const title = '俺はtodoアプリを作るぞ！';
 const App: React.FC = () => {
   return (
     <div className="App">
