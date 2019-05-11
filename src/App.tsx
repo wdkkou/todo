7559255df3dfec023a15d1525b51969399f2c24b
wdkkou <koukouki7547@gmail.com>
2019-05-11 20:03:00 +0900

    h1 tag 用意

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {FC} from 'react';
 import {Helmet} from 'react-helmet';
 import './App.css';
 
-const title = '俺はtodoアプリを作るぞ！';
-const App: React.FC = () => {
+const title = '俺はtodoアプリを作るぞ！！！！！！';
+const App: FC = () => {
   return (
-    <div className="App">
+    <>
       <Helmet htmlAttributes={{lang: 'ja'}}>
         <title>{title}</title>
       </Helmet>
-    </div>
+      <header className="App-header">
+        <h1>{title}</h1>
+      </header>
+    </>
   );
 };
 
