2e238e2685e129d3aefc7fdfaf3508b0ab5255d7
wdkkou <koukouki7547@gmail.com>
2019-07-05 00:49:52 +0900

    todosStateを用意

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,5 +1,37 @@
 import { Reducer } from 'redux';
+import { stat } from 'fs';
+import { TodoAction, TodoActionType } from './actions/actions';
 
-const todoReducer: Reducer = () => {};
+export interface TodoState {
+  text?: string;
+  id?: number;
+}
+export interface TodosState {
+  todos?: TodoState[];
+}
+export const initialState: TodosState = {
+  todos: [{ text: 'Lern redux', id: 0 }],
+};
+
+const todoReducer: Reducer<TodosState, TodoAction> = (
+  state,
+  action,
+): TodosState => {
+  switch (action.type) {
+    case TodoActionType.ADD:
+      return {
+        ...state,
+        todos: [{ text: action.text }],
+      };
+    case TodoActionType.DELETE:
+      return {
+        ...state,
+        todos: [{ id: action.id }],
+      };
+    default: {
+      return { ...state };
+    }
+  }
+};
 
 export default todoReducer;
