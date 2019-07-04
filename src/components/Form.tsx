df0fae8d6cc5126231b0f3d4f487d151ee76e252
wdkkou <koukouki7547@gmail.com>
2019-07-05 00:48:50 +0900

    form modified

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
-import React, {FC, SyntheticEvent, useState} from 'react';
-import {Form} from 'semantic-ui-react';
+import React, { FC, SyntheticEvent, useState } from 'react';
+import { Form } from 'semantic-ui-react';
 import './Form.css';
 
 interface FormProps {
@@ -7,20 +7,21 @@ interface FormProps {
   title?: string;
   desc?: string;
 }
-const FormTemplate: FC<FormProps> = ({create, title = '', desc = ''}) => {
+const FormTemplate: FC<FormProps> = ({ create, title = '', desc = '' }) => {
   const [Title, setTitle] = useState(title);
   const [Desc, setDesc] = useState(desc);
-  //入力がなされた際にstateを変更する関数（何か嫌だけどしょうがない）
+  // 入力がなされた際にstateを変更する関数（何か嫌だけどしょうがない）
   const changeInput = (e: SyntheticEvent) => {
     setTitle((e.target as HTMLInputElement).value);
   };
   const changeTextarea = (e: SyntheticEvent) => {
     setDesc((e.target as HTMLTextAreaElement).value);
   };
+
   return (
     <Form className="form-board" onSubmit={() => create(Title, Desc)}>
       <Form.Field>
-        <label className="todo-title">todo title</label>
+        {/* <label className="todo-title">todo title</label> */}
         <input
           name="title"
           placeholder="タイトル"
