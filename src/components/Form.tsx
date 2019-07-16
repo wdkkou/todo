4abb165de0a854625f87d941ef70c6ee580dc8e3
wdkkou <koukouki7547@gmail.com>
2019-07-17 01:23:19 +0900

    components Form syuusei

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
-import React, { FC, SyntheticEvent, useState } from 'react';
+// import React, { FC, SyntheticEvent, useState } from 'react';
+import React, { FC } from 'react';
 import { Form } from 'semantic-ui-react';
 import './Form.css';
 
@@ -7,38 +8,70 @@ interface FormProps {
   title?: string;
   desc?: string;
 }
-const FormTemplate: FC<FormProps> = ({ create, title = '', desc = '' }) => {
-  const [Title, setTitle] = useState(title);
-  const [Desc, setDesc] = useState(desc);
+
+export interface Todo {
+  id?: number;
+  title?: string;
+  description?: string;
+}
+
+interface TodoListProps {
+  TodoState?: Todo;
+  add?: (text: string) => void;
+  dele?: (id: number) => void;
+}
+// const FormTemplate: FC<FormProps> = ({ create, title = '', desc = '' }) => {
+//   const [Title, setTitle] = useState(title);
+//   const [Desc, setDesc] = useState(desc);
+//   // 入力がなされた際にstateを変更する関数（何か嫌だけどしょうがない）
+//   const changeInput = (e: SyntheticEvent) => {
+//     setTitle((e.target as HTMLInputElement).value);
+//   };
+//   const changeTextarea = (e: SyntheticEvent) => {
+//     setDesc((e.target as HTMLTextAreaElement).value);
+//   };
+
+//   return (
+//     <Form className="form-board" onSubmit={() => create(Title, Desc)}>
+//       <Form.Field>
+//         {/* <label className="todo-title">todo title</label> */}
+//         <input
+//           name="title"
+//           placeholder="タイトル"
+//           onChange={e => changeInput(e)}
+//         />
+//       </Form.Field>
+//       <Form.Field>
+//         <textarea
+//           name="desc"
+//           placeholder="詳細"
+//           onChange={e => changeTextarea(e)}
+//         />
+//       </Form.Field>
+//       {/* e.targetとかやったけどわからん */}
+//       <Form.Button content="Create" />
+//     </Form>
+//   );
+// };
+
+const FormTodo: FC<TodoListProps> = ({
+  add = () => {},
+  TodoState = { title: '', description: '' },
+}) => {
   // 入力がなされた際にstateを変更する関数（何か嫌だけどしょうがない）
-  const changeInput = (e: SyntheticEvent) => {
-    setTitle((e.target as HTMLInputElement).value);
-  };
-  const changeTextarea = (e: SyntheticEvent) => {
-    setDesc((e.target as HTMLTextAreaElement).value);
-  };
 
   return (
-    <Form className="form-board" onSubmit={() => create(Title, Desc)}>
+    <Form className="form-board" onSubmit={() => add(TodoState.title || '')}>
       <Form.Field>
         {/* <label className="todo-title">todo title</label> */}
-        <input
-          name="title"
-          placeholder="タイトル"
-          onChange={e => changeInput(e)}
-        />
+        <input name="title" placeholder="タイトル" />
       </Form.Field>
       <Form.Field>
-        <textarea
-          name="desc"
-          placeholder="詳細"
-          onChange={e => changeTextarea(e)}
-        />
+        <textarea name="desc" placeholder="詳細" />
       </Form.Field>
       {/* e.targetとかやったけどわからん */}
       <Form.Button content="Create" />
     </Form>
   );
 };
-
-export default FormTemplate;
+export default FormTodo;
