f07e90811fcae309b50acbdc3c58fa4d4dc31391
wdkkou <koukouki7547@gmail.com>
2019-07-17 01:22:44 +0900

    App container

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,17 @@
-import React, {FC, useState} from 'react';
-// import {Helmet} from 'react-helmet';
+import React, { FC } from 'react';
 import './App.css';
-import FormTemplate from './components/Form';
-import Todolist, {Todo} from './components/Todolist';
+import Form from './containers/Form';
 
 const title = 'reactでtodoアプリ';
 const App: FC = () => {
-  const todos: Todo[] = [
-    {
-      id: 1,
-      title: 'react',
-      description: 'typescriptの使い方',
-    },
-  ];
-  const [Todos, setTodos] = useState(todos);
-  const create = (t: string, d: string) => {
-    const data: Todo = {
-      id: Todos.length + 1,
-      title: t,
-      description: d,
-    };
-    setTodos([...Todos, data]);
-  };
-  const dele = (id: number) => {
-    Todos.splice(id, 1);
-    setTodos([...Todos]);
-  };
-
   return (
     <>
-      {/* <Helmet htmlAttributes={{lang: 'ja'}}> */}
       <title>{title}</title>
-      {/* </Helmet> */}
       <header className="App-header">
         <h1>{title}</h1>
       </header>
-      <FormTemplate create={create} />
-      <Todolist todos={Todos} dele={dele} />
+      <Form />
+      {/* <Todolist todos={Todos} dele={dele} /> */}
     </>
   );
 };
