17f770efecd23393d73bf97e1b1213c6d41f9d9f
wdkkou <koukouki7547@gmail.com>
2019-07-18 17:20:32 +0900

    components form todolist

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,14 +1,22 @@
 import React, { FC } from 'react';
 import { Table, Label, Button } from 'semantic-ui-react';
+import { TodosState } from '../reducer';
 import './Table.css';
 
-export interface Todo {
-  id: number;
-  title: string;
-  description?: string;
-}
+// export interface Todo {
+//   id: number;
+//   title: string;
+//   description?: string;
+// }
+
+// interface TodoListProps {
+//   todos: Todo[];
+//   dele: (id: number) => void;
+// }
+
 interface TodoListProps {
-  todos: Todo[];
+  // todos: Todo[];
+  todos: TodosState;
   dele: (id: number) => void;
 }
 
@@ -19,7 +27,7 @@ const Todolist: FC<TodoListProps> = ({ todos, dele }) => {
         <Table.Header>
           <Table.Row>
             <Table.HeaderCell>Title</Table.HeaderCell>
-            <Table.HeaderCell>Description</Table.HeaderCell>
+            {/* <Table.HeaderCell>Description</Table.HeaderCell> */}
           </Table.Row>
         </Table.Header>
         {todos.map((todo, index) => (
@@ -29,7 +37,7 @@ const Todolist: FC<TodoListProps> = ({ todos, dele }) => {
                 <Label>{todo.title}</Label>
               </Table.Cell>
               <Table.Cell>
-                {todo.description}
+                {/* {todo.description} */}
                 <Button
                   type="submit"
                   className="delete-button"
