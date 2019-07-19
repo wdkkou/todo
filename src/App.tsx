1716388617f882b554d02e7653807d69339dc785
wdkkou <koukouki7547@gmail.com>
2019-07-19 21:40:41 +0900

    不要なコードを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React, { FC } from 'react';
 import './App.css';
 import Form from './containers/Form';
+import Todolist from './containers/Todolist';
 
 const title = 'reactでtodoアプリ';
 const App: FC = () => {
   return (
     <>
-      <title>{title}</title>
       <header className="App-header">
         <h1>{title}</h1>
       </header>
       <Form />
-      {/* <Todolist todos={Todos} dele={dele} /> */}
+      <Todolist />
     </>
   );
 };
