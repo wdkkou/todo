5e5ef708deb53dd6d13a560a6170302eb4eb76f4
wdkkou <koukouki7547@gmail.com>
2019-07-21 03:56:38 +0900

    update src/App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react';
-import Helmet from 'react-helmet';
 import './App.css';
 import Form from './containers/Form';
 import Todolist from './containers/Todolist';
@@ -8,9 +7,7 @@ const title = 'reactでtodoアプリ';
 const App: FC = () => {
   return (
     <>
-      {/* <Helmet> */}
       <title> React todoアプリ</title>
-      {/* </Helmet> */}
       <header className="App-header">
         <h1>{title}</h1>
       </header>
