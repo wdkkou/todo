be091333af088d881fcea9a813f32439224b12c2
wdkkou <koukouki7547@gmail.com>
2019-10-26 19:56:09 +0900

    Todoコンポーネントを表示するように変更

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { Table, Label, Button } from 'semantic-ui-react';
 import { TodoState } from '../reducer';
+import Todo from './todo';
 import './Table.css';
 
 interface TodoListProps {
@@ -22,10 +23,14 @@ const Todolist: FC<TodoListProps> = ({ todos, dele }) => {
           <Table.Body key={todo.id}>
             <Table.Row>
               <Table.Cell>
-                <Label>{todo.text}</Label>
+                {/* <Label>{todo.text}</Label> */}
+                <Label>
+                  <Todo text={todo.text || ''} completed />
+                </Label>
               </Table.Cell>
               <Table.Cell>
-                {todo.description}
+                <Todo text={todo.description || ''} completed />
+                {/* {todo.description} */}
                 <Button
                   type="submit"
                   className="delete-button"
